f7758405f9168e4ff8c8468248451fce61b742c5
serg310583 <serg310583@mail.ru>
2022-12-07 19:54:36 +0700

    выполнено форматирование

--- script.js
+++ script.js
@@ -26,10 +26,15 @@ function initSlider() {
     let sliderDots = document.querySelector(".slider__dots");
     let sliderTitles = document.querySelector(".slider__titles");
 
+    let infoCity = document.querySelector(".block_1");
+    let infoRepairTime = document.querySelector(".block_2");
+    let infoArea = document.querySelector(".block_3")
+
     initImages();
     initArrows();
     initDots();
     initTitles();
+    initCards();
 
 
     function initImages() {
@@ -56,28 +61,41 @@ function initSlider() {
 
     function initDots() {
         images.forEach((images, index) => {
-          let dot = `<div class="slider__dots-item n${index} ${index === 0? "active" : ""}" data-index="${index}"></div>`;
-          sliderDots.innerHTML += dot;
+            let dot = `<div class="slider__dots-item n${index} ${index === 0? "active" : ""}" data-index="${index}"></div>`;
+            sliderDots.innerHTML += dot;
         });
+
         sliderDots.querySelectorAll(".slider__dots-item").forEach(dot => {
-          dot.addEventListener("click", function() {
-            moveSlider(this.dataset.index);            
-          });
+            dot.addEventListener("click", function() {
+                moveSlider(this.dataset.index);            
+            });
         });
     }
 
     function initTitles() {
         images.forEach((image, index) => {
-          let title = `<div class="slider__title-item n${index} ${index === 0? "active" : ""}" data-index="${index}">${images[index].title}</div>`;
-          sliderTitles.innerHTML += title;
+            let title = `<div class="slider__title-item n${index} ${index === 0? "active" : ""}" data-index="${index}">${images[index].title}</div>`;
+            sliderTitles.innerHTML += title;
         });
+
         sliderTitles.querySelectorAll(".slider__title-item").forEach(title => {
-          title.addEventListener("click", function() {
+            title.addEventListener("click", function() {
             moveSlider(this.dataset.index);            
-          });
+            });
         });
     }
-    
+
+    function initCards() {
+        images.forEach((image, index) => {
+            let city = `<div class="text n${index} ${index === 0 ? "text-active" : ""}" data-index="${index}">${images[index].city}</div>`;
+            infoCity.innerHTML += city;
+            let area = `<div class="text n${index} ${index === 0 ? "text-active" : ""}" data-index="${index}">${images[index].area}</div>`;
+            infoArea.innerHTML += area;
+            let time = `<div class="text n${index} ${index === 0 ? "text-active" : ""}" data-index="${index}">${images[index].time}</div>`;
+            infoRepairTime.innerHTML += time;   
+        })         
+    }   
+     
     function moveSlider(num) {
         sliderImages.querySelector(".active").classList.remove("active");
         sliderImages.querySelector(".n" + num).classList.add("active");
@@ -87,9 +105,15 @@ function initSlider() {
 
         sliderTitles.querySelector(".active").classList.remove("active");
         sliderTitles.querySelector(".n" + num).classList.add("active");
+
+        infoCity.querySelector(".text-active").classList.remove("text-active");
+        infoCity.querySelector(".n" + num).classList.add("text-active");
+
+        infoArea.querySelector(".text-active").classList.remove("text-active");
+        infoArea.querySelector(".n" + num).classList.add("text-active");
+
+        infoRepairTime.querySelector(".text-active").classList.remove("text-active");
+        infoRepairTime.querySelector(".n" + num).classList.add("text-active");
     }    
 }
-
-    
-
 document.addEventListener("DOMContentLoaded", initSlider);
